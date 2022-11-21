6ccd3778fc1e59d9f9dfbad8360c65db61c6ca86
YuYF97 <60455450+YuYF97@users.noreply.github.com>
2022-11-21 21:13:44 +0800

    Update example.R

--- example.R
+++ example.R
@@ -1,11 +1,8 @@
-library(bnlearn)
 source("simulation_function.R")
 source("MPPC.R")
 
-load('arth150.rda')
-arth150 <- rbn(bn, n = 10000)
-
-bnn <- tranbn(bn)
-know <- knowledge(bnn, p.d = 0.3, p.w = 0.3, p.b = 0.3)
-mppc <- mpbn(data = magic, M.whitelist = know$knowledge.white, 
+load("arth.RData")
+load("arth_know.RData")
+mppc <- mpbn(data = arth, M.whitelist = know$knowledge.white, 
              M.blacklist = know$knowledge.black)
+
