3ba4808c285333d8307f5b231908fd37b8f8b2f9
YuYF97 <60455450+YuYF97@users.noreply.github.com>
2023-04-28 22:36:21 +0800

    Update unshield_triple.cpp

--- unshield_triple.cpp
+++ unshield_triple.cpp
@@ -9,11 +9,13 @@ using namespace Rcpp;
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 
-RcppExport SEXP triple(List und_arc, List arc) {
+RcppExport SEXP triple(List und_arc, List arc, List dir_arc) {
   std::vector<std::string> ufrom = und_arc["from"];
   std::vector<std::string> uto = und_arc["to"];
   std::vector<std::string> afrom = arc["from"];
   std::vector<std::string> ato = arc["to"];
+  std::vector<std::string> dfrom = dir_arc["from"];
+  std::vector<std::string> dto = dir_arc["to"];
   
   std::vector<std::string> from;
   std::vector<std::string> med;
@@ -72,19 +74,38 @@ RcppExport SEXP triple(List und_arc, List arc) {
         }
       }
     }
-    
-    for (int i=0; i<from.size(); i++) {
-      std::string f = from.at(i);
-      std::string t = to.at(i);
-      for (int j=0; j<afrom.size(); j++) {
-        if ((f.compare(afrom.at(j))==0 & t.compare(ato.at(j))==0) | 
-            (f.compare(ato.at(j))==0 & t.compare(afrom.at(j))==0)) {
-          from.erase(from.begin()+i);
-          med.erase(med.begin()+i);
-          to.erase(to.begin()+i);
-          i=i-1;
-          break;
-        }
+  }
+  
+  if (ufrom.size()>0) {
+    for (int i=0; i<ufrom.size(); i++) {
+      std::string f = ufrom.at(i);
+      std::string t = uto.at(i);
+      
+      for (int j=0; j<dfrom.size(); j++) {
+        if (f.compare(dto.at(j))==0) {
+          med.push_back(f);
+          from.push_back(dfrom.at(j));
+          to.push_back(t);
+        } else if (t.compare(dto.at(j))==0) {
+          med.push_back(t);
+          from.push_back(dfrom.at(j));
+          to.push_back(f);
+        } 
+      }
+    }
+  }
+  
+  for (int i=0; i<from.size(); i++) {
+    std::string f = from.at(i);
+    std::string t = to.at(i);
+    for (int j=0; j<afrom.size(); j++) {
+      if ((f.compare(afrom.at(j))==0 & t.compare(ato.at(j))==0) |
+          (f.compare(ato.at(j))==0 & t.compare(afrom.at(j))==0)) {
+        from.erase(from.begin()+i);
+        med.erase(med.begin()+i);
+        to.erase(to.begin()+i);
+        i=i-1;
+        break;
       }
     }
   }
